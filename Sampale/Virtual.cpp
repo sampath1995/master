5ecfe2195f39457fbe1bb725907f5e7919907a66
sampath1995 <61143982+sampath1995@users.noreply.github.com>
2020-02-25 15:38:34 +0100

    Added Files through Visual Studio (CPP Files)

--- Sampale/Virtual.cpp
+++ Sampale/Virtual.cpp
@@ -1,13 +1,59 @@
 #include<iostream>
 #include<string>
+#include<memory>
+
+
+
 class Entity {
+private:
+	std::string s_Name;
 public:
+
 	std::string getName() {
 		return "Entity";
 	}
 
+	void printname(const std::string& name) {
+		this->s_Name = name;
+		std::cout << s_Name << std::endl;
+	};
+
 };
 
+class UniqPointer {
+private:
+	std::string Uniqpoint;
+public:
+	UniqPointer() {
+		std::cout << "Unique pointer is created" << std::endl;
+	};
+	~UniqPointer() {
+		std::cout << "Gone Went Gone - Unique Pointer" << std::endl;
+	}
+};
+
+class SharedPointer {
+private:
+	std::string Sharedpoint;
+public:
+	SharedPointer() {
+		std::cout << "shared pointer is created" << std::endl;
+	};
+	~SharedPointer() {
+		std::cout << "Gone Went Gone - Shared Pointer" << std::endl;
+	}
+};
+class WeakPointer {
+private:
+	std::string Uniqpoint;
+public:
+	WeakPointer() {
+		std::cout << "Weak pointer is created" << std::endl;
+	};
+	~WeakPointer() {
+		std::cout << "Gone Went Gone - Weak Pointer" << std::endl;
+	}
+};
 class Player : public Entity {
 private:
 	std::string m_player;
@@ -18,14 +64,58 @@ public:
 
 };
 
-
+void testMyAsset(const Entity& Lo) {
+	std::cout << "Sike" << std::endl;
+}
 int main() {
 
-
+	{
 	Entity* e = new Entity();
 	std::cout << e->getName() << std::endl;
 
 	Player* p = new Player("CASSINI");
 	std::cout << p->getName() << std::endl;
+
+	std::unique_ptr<Entity> entity1(new Entity());
+
+	entity1->printname("Sampath");
+
+	}
+	//testMyAsset(*entity1); - the object is deleted because it is out of scope 
+
+	// Unique pointers
+	{
+		std::unique_ptr<UniqPointer> uni(new UniqPointer());
+		{
+
+		}
+	}
+
+	
+	//shared pointers'
+	{
+	std::shared_ptr<SharedPointer> shareCB3;
+
+	{ 
+		std::shared_ptr<SharedPointer> shareCB2;
+		{
+			std::shared_ptr<SharedPointer> shared = std::make_shared<SharedPointer>();
+			shareCB2 = shared;
+		}
+		shareCB3 = shareCB2;
+
+	}
+	}
+	//weak pointers
+	{
+		{
+			//std::weak_ptr<WeakPointer> weak = std::make_
+			//for weak pointer we just dont count blocks;
+			//we just check wheather the pointer is alive or not
+
+		}
+	}
+
+	
 	std::cin.get();
 }
